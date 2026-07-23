c5f3e2f798b656e6d95a6f837f79d71ec0d5c232
solairerove <solairerove@gmail.com>
2026-07-23 11:33:44 +0700

    Add Problem 169: Implement `majority_element` with test cases and update module exports

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -3,6 +3,7 @@ pub mod p0014_longest_common_prefix;
 pub mod p0021_merge_two_sorted_lists;
 pub mod p0027_remove_element;
 pub mod p0049_group_anagrams;
+pub mod p0169_majority_element;
 pub mod p0206_reverse_linked_list;
 pub mod p0217_contains_duplicate;
 pub mod p0242_valid_anagram;
---
+++ src/problems/p0169_majority_element.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+
+// time O(n), space O(n)
+pub fn majority_element(nums: Vec<i32>) -> i32 {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+
+    let mut cnt: HashMap<i32, i32> = HashMap::new();
+    let mut res = 0;
+    let mut max = 0;
+    for num in nums {
+        if cnt.contains_key(&num) {
+            if let Some(v) = cnt.get_mut(&num) {
+                *v += 1;
+                if *v > max {
+                    res = num;
+                    max = *v;
+                }
+            }
+        } else {
+            cnt.insert(num, 0);
+        }
+    }
+
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(majority_element(vec![3, 2, 3]), 3);
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(majority_element(vec![2, 2, 1, 1, 1, 2, 2]), 2);
+    }
+
+    #[test]
+    fn handles_single() {
+        assert_eq!(majority_element(vec![1]), 1);
+    }
+}
