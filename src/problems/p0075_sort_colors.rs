f1dfb3a6d3c63231e863f7ee9e7bf4ea695c30c6
solairerove <solairerove@gmail.com>
2026-07-26 11:55:09 +0700

    Add Problem 75: Implement `sort_colors` with test cases and update module exports

---
+++ src/problems/p0075_sort_colors.rs
@@ -0,0 +1,110 @@
+// Dutch National Flag algorithm
+// time O(n), space O(1)
+pub fn sort_colors(nums: &mut Vec<i32>) {
+    let mut i: i32 = 0;
+    let mut l: i32 = 0;
+    let mut r: i32 = nums.len() as i32 - 1;
+    while i <= r {
+        if nums[i as usize] == 0 {
+            nums.swap(l as usize, i as usize);
+            l += 1;
+        } else if nums[i as usize] == 2 {
+            nums.swap(i as usize, r as usize);
+            r -= 1;
+            i -= 1;
+        }
+        i += 1;
+    }
+}
+
+// time O(n), space O(1)
+pub fn sort_colors_bucket(nums: &mut Vec<i32>) {
+    let mut cnt = [0; 3];
+    for &num in nums.iter() {
+        cnt[num as usize] += 1;
+    }
+
+    let mut idx: usize = 0;
+    for i in 0..3 {
+        while cnt[i] > 0 {
+            cnt[i] -= 1;
+            nums[idx] = i as i32;
+            idx += 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mixed_colors() {
+        let mut nums = vec![2, 0, 2, 1, 1, 0];
+        sort_colors(&mut nums);
+        assert_eq!(nums, vec![0, 0, 1, 1, 2, 2]);
+    }
+
+    #[test]
+    fn test_already_sorted() {
+        let mut nums = vec![0, 1, 2];
+        sort_colors(&mut nums);
+        assert_eq!(nums, vec![0, 1, 2]);
+    }
+
+    #[test]
+    fn test_single_element() {
+        let mut nums = vec![1];
+        sort_colors(&mut nums);
+        assert_eq!(nums, vec![1]);
+    }
+
+    #[test]
+    fn test_all_same_color() {
+        let mut nums = vec![1, 1, 1];
+        sort_colors(&mut nums);
+        assert_eq!(nums, vec![1, 1, 1]);
+    }
+
+    #[test]
+    fn test_reverse_sorted() {
+        let mut nums = vec![2, 1, 0];
+        sort_colors(&mut nums);
+        assert_eq!(nums, vec![0, 1, 2]);
+    }
+
+    #[test]
+    fn test_mixed_colors_bucket() {
+        let mut nums = vec![2, 0, 2, 1, 1, 0];
+        sort_colors_bucket(&mut nums);
+        assert_eq!(nums, vec![0, 0, 1, 1, 2, 2]);
+    }
+
+    #[test]
+    fn test_already_sorted_bucket() {
+        let mut nums = vec![0, 1, 2];
+        sort_colors_bucket(&mut nums);
+        assert_eq!(nums, vec![0, 1, 2]);
+    }
+
+    #[test]
+    fn test_single_element_bucket() {
+        let mut nums = vec![1];
+        sort_colors_bucket(&mut nums);
+        assert_eq!(nums, vec![1]);
+    }
+
+    #[test]
+    fn test_all_same_color_bucket() {
+        let mut nums = vec![1, 1, 1];
+        sort_colors_bucket(&mut nums);
+        assert_eq!(nums, vec![1, 1, 1]);
+    }
+
+    #[test]
+    fn test_reverse_sorted_bucket() {
+        let mut nums = vec![2, 1, 0];
+        sort_colors_bucket(&mut nums);
+        assert_eq!(nums, vec![0, 1, 2]);
+    }
+}
