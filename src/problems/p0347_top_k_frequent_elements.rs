67402ffa0314cab493f870b0ed5cd261ac31e9e7
solairerove <solairerove@gmail.com>
2026-07-30 12:56:57 +0700

    Add Problem 347: Refactor `top_k_frequent` with hash map-based frequency counting and heap; include new tests for edge cases

--- src/problems/p0347_top_k_frequent_elements.rs
+++ src/problems/p0347_top_k_frequent_elements.rs
@@ -1,8 +1,30 @@
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
+use std::collections::HashMap;
+
+// time O(n log k), space O(n)
+pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let k = k as usize;
+    let mut count = HashMap::new();
+    for &num in &nums {
+        *count.entry(num).or_insert(0i32) += 1;
+    }
+
+    let mut heap = BinaryHeap::new();
+    for (&num, &freq) in &count {
+        heap.push(Reverse((freq, num)));
+        if heap.len() > k {
+            heap.pop();
+        }
+    }
+
+    heap.into_iter().map(|Reverse((_, num))| num).collect()
+}
+
+// TBD: quickselect with Dutch national flag partition
 
 // time O(n + span log k), space O(span + k)
-pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+pub fn top_k_frequent_bucket_heap(nums: Vec<i32>, k: i32) -> Vec<i32> {
     let Some((min, max)) = min_max(&nums) else {
         return nums;
     };
@@ -91,6 +113,35 @@ mod tests {
     #[test]
     fn all_unique() {
         let res = top_k_frequent(vec![5, 3, 1, 9], 4);
+        assert_eq!(as_set(res), as_set(vec![5, 3, 1, 9]));
+    }
+
+    #[test]
+    fn bucket_heap_example_case() {
+        let res = top_k_frequent_bucket_heap(vec![1, 1, 1, 2, 2, 3], 2);
+        assert_eq!(as_set(res), as_set(vec![1, 2]));
+    }
+
+    #[test]
+    fn bucket_heap_single_element() {
+        assert_eq!(top_k_frequent_bucket_heap(vec![1], 1), vec![1]);
+    }
+
+    #[test]
+    fn bucket_heap_k_equals_two() {
+        let res = top_k_frequent_bucket_heap(vec![4, 1, -1, 2, -1, 2, 3], 2);
+        assert_eq!(as_set(res), as_set(vec![-1, 2]));
+    }
+
+    #[test]
+    fn bucket_heap_negative_numbers() {
+        let res = top_k_frequent_bucket_heap(vec![-1, -1, -2, -2, -2, -3], 2);
+        assert_eq!(as_set(res), as_set(vec![-2, -1]));
+    }
+
+    #[test]
+    fn bucket_heap_all_unique() {
+        let res = top_k_frequent_bucket_heap(vec![5, 3, 1, 9], 4);
         assert_eq!(as_set(res), as_set(vec![5, 3, 1, 9]));
     }
 }
